978834a66db5f872ecb9bbeb20ec2f78d5132a6c
Markus Staab <maggus.staab@googlemail.com>
2023-02-06 16:09:18 +0100

    Restore src/Type/Accessory/OversizedArrayType.php

--- src/Type/Accessory/OversizedArrayType.php
+++ src/Type/Accessory/OversizedArrayType.php
@@ -4,7 +4,6 @@ namespace PHPStan\Type\Accessory;
 
 use PHPStan\TrinaryLogic;
 use PHPStan\Type\AcceptsResult;
-use PHPStan\Type\BooleanType;
 use PHPStan\Type\CompoundType;
 use PHPStan\Type\Constant\ConstantFloatType;
 use PHPStan\Type\Constant\ConstantIntegerType;
@@ -325,11 +324,6 @@ class OversizedArrayType implements CompoundType, AccessoryType
 	public function isScalar(): TrinaryLogic
 	{
 		return TrinaryLogic::createNo();
-	}
-
-	public function looseCompare(Type $type): BooleanType
-	{
-		return $type->isOversizedArray()->toBooleanType();
 	}
 
 	public function toNumber(): Type
