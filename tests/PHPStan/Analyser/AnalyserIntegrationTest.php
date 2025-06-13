31ea32ff7ce295aed99dfd7c758e31af8ab42610
Ondrej Mirtes <ondrej@mirtes.cz>
2025-06-13 10:40:59 +0200

    Fix test

--- tests/PHPStan/Analyser/AnalyserIntegrationTest.php
+++ tests/PHPStan/Analyser/AnalyserIntegrationTest.php
@@ -184,7 +184,6 @@ class AnalyserIntegrationTest extends PHPStanTestCase
 
 	public function testCollectWarnings(): void
 	{
-		restore_error_handler();
 		$errors = $this->runAnalyse(__DIR__ . '/data/declaration-warning.php');
 		$this->assertCount(1, $errors);
 		$this->assertSame('Parameter #1 $i of method DeclarationWarning\Bar::doFoo() is not optional.', $errors[0]->getMessage());
