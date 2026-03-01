276251993d576ee0a6e08c46a4b5d8fd9dc6731f
Markus Staab <maggus.staab@googlemail.com>
2026-03-01 20:38:09 +0100

    implement montly markdown reporting

---
+++ month-report.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once 'report-classes.php';
+
+$author = 'staabm';
+$authorHandle = '@staabm';
+$fullName = 'Markus Staab';
+echo "Month: February\n";
+
+// gh pr list --repo phpstan/phpstan-src --state merged --limit 50 --json number,title,author,createdAt,reviews,url,body --search 'sort:updated-desc is:pr author:staabm created:2026-02-01..2026-02-28' > staabm-prs-feb.json
+$json = file_get_contents('staabm-prs-feb.json');
+$result = json_decode($json, true);
+
+printf("## %s Pull requests authored by %s\n", count($result), $fullName);
+foreach($result as $pr) {
+	printf("- [#%s](%s) %s\n", $pr['number'], $pr['url'], $pr['title']);
+
+	$references = KeywordsParser::findReferencedIssues($pr['url'], $pr['body']);
+	foreach($references as $reference) {
+		printf ("  - Fixes [%s](%s) \n", $reference->issueRef, $reference->getUrl());
+	}
+}
+
+
+// gh pr list --repo phpstan/phpstan-src --state merged --limit 500 --json number,title,author,createdAt,reviews,url,body --search 'sort:updated-desc is:pr reviewed-by:staabm -author:staabm merged:2026-02-01..2026-02-28' > staabm-reviewed-prs-feb.json
+$json = file_get_contents('staabm-reviewed-prs-feb.json');
+$result = json_decode($json, true);
+
+printf("## %s Pull requests reviewed by %s\n", count($result), $fullName);
+foreach($result as $pr) {
+	printf("- [#%s](%s) %s authored by %s\n", $pr['number'], $pr['url'], $pr['title'], $pr['author']['name']);
+
+	$references = KeywordsParser::findReferencedIssues($pr['url'], $pr['body']);
+	foreach($references as $reference) {
+		printf ("  - Fixes [%s](%s) \n", $reference->issueRef, $reference->getUrl());
+	}
+}
+
---
+++ report-classes.php
@@ -0,0 +1,123 @@
+<?php
+
+enum DescriptionKeyword: string
+{
+	case CLOSE = 'close';
+	case CLOSES = 'closes';
+	case CLOSED = 'closed';
+	case FIX = 'fix';
+	case FIXES = 'fixes';
+	case FIXED = 'fixed';
+	case RESOLVE = 'resolve';
+	case RESOLVES = 'resolves';
+	case RESOLVED = 'resolved';
+}
+
+final class KeywordsParser
+{
+	/**
+	 * @return list<IssueReference>
+	 */
+	static public function findReferencedIssues(string $prUrl, string $bodyText): array
+	{
+		$keywords = [];
+		foreach(DescriptionKeyword::cases() as $case) {
+			$keywords[] = $case->value;
+		}
+
+		// find "closes phpstan/phpstan#10169"
+		$matches = [];
+		preg_match_all('{('. implode('|', $keywords) .')\s+([a-z]+/[a-z]+#[0-9]+)}i', $bodyText, $matches);
+
+		$issues = [];
+		foreach($matches[0] as $i => $match) {
+			$issues[] = new IssueReference(
+				DescriptionKeyword::from(strtolower($matches[1][$i])),
+				$matches[2][$i]
+			);
+		}
+
+		// find urls "closes https://github.com/phpstan/phpstan/issues/8366"
+		$matches = [];
+		preg_match_all('{('. implode('|', $keywords) .')\s+https://github.com/([a-z]+/[a-z]+)/issues/([0-9]+)}i', $bodyText, $matches);
+
+		foreach($matches[0] as $i => $match) {
+			$issues[] = new IssueReference(
+				DescriptionKeyword::from(strtolower($matches[1][$i])),
+				$matches[2][$i].'#'.$matches[3][$i]
+			);
+		}
+
+		// find relative references "closes #597"
+		$matches = [];
+		preg_match_all('{('. implode('|', $keywords) .')\s+(#[0-9]+)}i', $bodyText, $matches);
+
+		$urlParser = new PullRequestUrlParser($prUrl);
+		foreach($matches[0] as $i => $match) {
+			$issues[] = new IssueReference(
+				DescriptionKeyword::from(strtolower($matches[1][$i])),
+				$urlParser->getRepoIdentifier().$matches[2][$i]
+			);
+		}
+
+		return $issues;	}
+}
+
+
+
+
+readonly class IssueReference
+{
+	public function __construct(
+		public DescriptionKeyword $keyword,
+		public string $issueRef // e.g. "phpstan/phpstan#10169"
+	)
+	{
+	}
+
+	public function getUrl(): string
+	{
+		return 'https://github.com/' . $this->issueRef;
+	}
+
+	public function getRepoOwner(): string {
+		$prefix = explode('#', $this->issueRef)[0];
+
+		return explode('/', $prefix)[0];
+	}
+
+	public function getRepoName(): string {
+		$prefix = explode('#', $this->issueRef)[0];
+
+		return explode('/', $prefix)[1];
+	}
+
+	public function getRepoIdentifier(): string {
+		$prefix = explode('#', $this->issueRef)[0];
+
+		return $prefix;
+	}
+
+	public function getNumber(): int {
+		return (int) explode('#', $this->issueRef)[1];
+	}
+}
+
+
+class PullRequestUrlParser {
+	public function __construct(
+		private string $url
+	) {
+	}
+
+	public function getRepoIdentifier(): string {
+		$path = parse_url($this->url, PHP_URL_PATH);
+		if (!is_string($path)) {
+			throw new \RuntimeException('Could not parse url: ' . $this->url);
+		}
+		$parts = explode('/', $path);
+
+		return $parts[1] . '/' . $parts[2];
+	}
+
+}
